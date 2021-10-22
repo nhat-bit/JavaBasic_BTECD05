f63cfd64cca292091f6195a1c3e2eee51e631a90
Nhat <dolongnhatnt@gmail.com>
2021-10-22 16:31:28 +0700

    Initial commit

---
+++ src/main/java/B2_Var_types_control_loop/Main_HellloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package B2_Var_types_control_loop;
+
+/**
+ *
+ * @author PC
+ */
+public class Main_HellloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Btec-D05-K12");
+    }
+    
+}
