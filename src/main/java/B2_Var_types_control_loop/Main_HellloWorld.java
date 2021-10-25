4948a0446c7747c8d1a21d616236b5d53010bb74
Nhat <dolongnhatnt@gmail.com>
2021-10-25 20:35:13 +0700

    Java Method

--- src/main/java/B2_Var_types_control_loop/Main_HellloWorld.java
+++ src/main/java/B2_Var_types_control_loop/Main_HellloWorld.java
@@ -2,20 +2,50 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package B2_Var_types_control_loop;
+package B02_var_types_control_loops;
 
 /**
  *
  * @author PC
  */
-public class Main_HellloWorld {
+public class Main_HelloWorld {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
         System.out.println("Hello Btec-D05-K12");
+
+        int s = sum(2, 3);
+        System.out.println(s);
+
+        System.out.println(sum(4, 6));
+
+        System.out.println(sum(3.5, 2.5));
+
+        System.out.println(sum(3));
     }
-    
+
+    private static int sum(int a, int b) {
+
+        System.out.println("int version");
+        return a + b;
+    }
+
+    // overloading method
+    private static double sum(double a, double b) {
+
+        System.out.println("double version");
+        return a + b;
+    }
+
+    // recursion (1 + 2 + ... + k)
+    public static int sum(int k) {
+        if (k > 0) {
+            return k + sum(k - 1);
+        } else {
+            return 0;
+        }
+    }
 }
